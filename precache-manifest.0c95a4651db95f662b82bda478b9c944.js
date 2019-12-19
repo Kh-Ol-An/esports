643123937bfdb0adc63b73ee87df4a1d095579b1
Kh-Ol-An <olegkhristenko@gmail.com>
2019-12-19 10:12:38 +0200

    Updates

---
+++ precache-manifest.0c95a4651db95f662b82bda478b9c944.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4793b6f03056574b0ba25102337d1885",
+    "url": "/esports/index.html"
+  },
+  {
+    "revision": "5fb2f3abbdfb53878f0c",
+    "url": "/esports/static/css/main.2cc89947.chunk.css"
+  },
+  {
+    "revision": "a4cc727344a63a61d782",
+    "url": "/esports/static/js/2.d7ea4bdf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/esports/static/js/2.d7ea4bdf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fb2f3abbdfb53878f0c",
+    "url": "/esports/static/js/main.977ea775.chunk.js"
+  },
+  {
+    "revision": "3724b5c1658f183d85f1",
+    "url": "/esports/static/js/runtime-main.ccb527ad.js"
+  },
+  {
+    "revision": "71bb2ed28cfa627c7c676da377bd40c6",
+    "url": "/esports/static/media/arrow.71bb2ed2.svg"
+  },
+  {
+    "revision": "eb19c076f9e7918940f3cf823c8ac0a4",
+    "url": "/esports/static/media/bottom-arrow.eb19c076.svg"
+  },
+  {
+    "revision": "2b6058c27e73ad3d84eafbdd3627b5fe",
+    "url": "/esports/static/media/filter.2b6058c2.svg"
+  },
+  {
+    "revision": "60792b0ef3568cc940ed6fc26a86a747",
+    "url": "/esports/static/media/lock.60792b0e.svg"
+  },
+  {
+    "revision": "b543f11557c1e25223b0b728a7b10cf3",
+    "url": "/esports/static/media/pagination-arrow.b543f115.svg"
+  },
+  {
+    "revision": "3b264f2a2714d19ca1c71ef21a1b32cc",
+    "url": "/esports/static/media/shield.3b264f2a.svg"
+  },
+  {
+    "revision": "bab17f1d48e24e9a5a5c18d5c864d41a",
+    "url": "/esports/static/media/top-arrow.bab17f1d.svg"
+  }
+]);
